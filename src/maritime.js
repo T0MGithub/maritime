e6fc81ce75453fa85c94806b1c135a0e7be94078
Tom <emailfortomg@gmail.com>
2020-01-14 20:46:30 +0000

    Started Data Structures

--- src/maritime.js
+++ src/maritime.js
@@ -1,16 +1,36 @@
 const http = require("http");
+const middlewareCompiler = require("./middlewareCompiler.js");
+const dataObj = require("./data/data.js");
+const requestObj = request("./data/req.js");
+const responseObj = require("./data/res.js");
 
 module.exports = class Maritime {
   constructor() {
     this.globalMiddleware = [];
   }
 
+  use(middleware) {
+    this.globalMiddleware.push(middleware);
+  }
+
+  handleServer() {
+    // function to pass to http server
+    return function(req, res) {
+      const data = this.constructData(req, res);
+      return this.handleRequest(data, middleware);
+    }
+  }
+
   handleRequest() {
     
   }
 
+  constructData() {
+
+  }
+
   listen(...args) {
-    const server = http.createServer(this.handleRequest());
+    const server = http.createServer(this.handleServer());
     return server.listen(...args);
   }
 };
