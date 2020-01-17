b9bca098e12e2c79e77ef645f776d31c65a00055
Tom <emailfortomg@gmail.com>
2020-01-17 17:21:19 +0000

    Added Support for "*" for wildcard routes

--- src/router/route.js
+++ src/router/route.js
@@ -1,8 +1,26 @@
 const { pathToRegexp, match, parse, compile } = require("path-to-regexp");
 
+const convertWildcards = function(path) {
+  let char;
+  for (let i = 0; i < path.length; i++) {
+    char = path[i];
+    if (char === "*") {
+      if (
+        (path[i + 1] == "/" || i === path.length - 1) &&
+        (path[i - 1] == "/" || i === 0)
+      ) {
+        path = path.substring(0, i) + "(.*)" + path.substring(i + 1);
+        i += 4;
+      }
+    }
+  }
+
+  return path;
+};
+
 function Route(methods, path, middleware, options) {
   this.methods = methods;
-  this.path = path;
+  this.path = convertWildcards(path);
   this.middleware = middleware;
   this.options = options || {};
   this.parameters = [];
@@ -16,6 +34,7 @@ Route.prototype.match = function(path) {
 
 Route.prototype.rebaseRoute = function(routeBase) {
   if (this.path) {
+    this.path = convertWildcards(this.path);
     this.path = routeBase + this.path;
     this.parameters = [];
     this.regex = pathToRegexp(this.path, this.parameters, this.options);
