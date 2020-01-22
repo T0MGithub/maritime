cfd0581105bb6b3c0efc42687d95476442749dfd
Tom <emailfortomg@gmail.com>
2020-01-22 18:25:02 +0000

    Display Runkit Endpoint if in a Runkit environment

--- example.js
+++ example.js
@@ -3,8 +3,14 @@ const app = new Maritime();
 const router = new Maritime.router();
 
 router.get("*", data => {
-  data.res.send("ok");
+  data.res.send("Hello World!");
 });
 
 app.mount(router);
-app.listen(3000);
+app.listen(3000, function() {
+  let msg =
+    process.env.RUNKIT_ENDPOINT_URL !== undefined
+      ? `Server running on port 3000! You can visit ${process.env.RUNKIT_ENDPOINT_URL} to see the server in action!`
+      : "Server running on port 3000!";
+  console.log(msg);
+});
