c9fe1997f0da21fc1c7b489bc56d5a09b3ec4831
Tom <emailfortomg@gmail.com>
2020-01-24 20:18:49 +0000

    Improved Runkit Example
    - Moved process.env.RUNKIT_ENDPOINT_URL to end of file, so it is at the top of the output in runkit

--- example.js
+++ example.js
@@ -9,8 +9,9 @@ router.get("*", data => {
 app.mount(router);
 app.listen(3000, function() {
   console.log("Server running on port 3000!");
-  if (process.env.RUNKIT_ENDPOINT_URL)
-    console.log(
-      `You can visit ${process.env.RUNKIT_ENDPOINT_URL} to see the server in action!`
-    );
 });
+
+if (process.env.RUNKIT_ENDPOINT_URL)
+  console.log(
+    `You can visit ${process.env.RUNKIT_ENDPOINT_URL} to see the server in action!`
+  );
