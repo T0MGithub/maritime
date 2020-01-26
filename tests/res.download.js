b683cc72db0acbc88e97325dab409d0876080611
Tom <emailfortomg@gmail.com>
2020-01-26 19:09:05 +0000

    Refactored Tests to Avoid Port Collisions

--- tests/res.download.js
+++ tests/res.download.js
@@ -4,16 +4,14 @@ const request = require("supertest");
 
 describe("req", function() {
   describe(".download(path)", function() {
-    it("should transfer as an attachment", function(done) {
-      var app = new Maritime();
+    it("should transfer file as an attachment", function(done) {
+      const app = new Maritime();
 
       app.use(function(data) {
         data.res.download(__dirname + "/fixtures/test.txt");
       });
 
-      var server = app.listen(3000);
-
-      request(server)
+      request(app.listen())
         .get("/")
         .expect("Content-Type", "text/plain; charset=utf-8")
         .expect("Content-Disposition", "attachment; filename=test.txt", done);
