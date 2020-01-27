ae23d7241e379bd6f73bcc26c994c315aedeea55
Tom <emailfortomg@gmail.com>
2020-01-27 19:12:32 +0000

    Added Test for .download() with alternative name

--- tests/res.download.js
+++ tests/res.download.js
@@ -17,4 +17,19 @@ describe("req", function() {
         .expect("Content-Disposition", "attachment; filename=test.txt", done);
     });
   });
+
+  describe(".download(path, altName)", function() {
+    it("should transfer attachment with alt name", function(done) {
+      const app = new Maritime();
+
+      app.use(function(data) {
+        data.res.download(__dirname + "/fixtures/test.txt", "test2.txt");
+      });
+
+      request(app.listen())
+        .get("/")
+        .expect("Content-Type", "text/plain; charset=UTF-8")
+        .expect("Content-Disposition", "attachment; filename=test2.txt", done);
+    });
+  });
 });
