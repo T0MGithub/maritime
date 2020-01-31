b2ee0b9e2e7cbebb6086ded92ee13ff8c1d139e5
Tom <emailfortomg@gmail.com>
2020-01-31 17:01:59 +0000

    Added JSDoc to Utils Functions

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,10 @@
 const contentType = require("content-type");
 
+/**
+ * Function to return list of standard HTTP verbs.
+ * 
+ * @return {List} List of standard HTTP verbs.
+ */
 module.exports.nodeHttpVerbs = () => [
   "get",
   "post",
@@ -29,6 +34,13 @@ module.exports.nodeHttpVerbs = () => [
   "connect"
 ];
 
+/**
+ * Function to add a getter function to an object.
+ * 
+ * @param {Object} [object] Object to add the getter function to.
+ * @param {String} [name] Name of getter function to add.
+ * @param {Function} [getterFunction] Actual getter function. 
+ */
 module.exports.addGetter = function(object, name, getterFunction) {
   Object.defineProperty(object, name, {
     get: getterFunction,
@@ -37,6 +49,13 @@ module.exports.addGetter = function(object, name, getterFunction) {
   });
 };
 
+/**
+ * Function to add a setter function to an object.
+ * 
+ * @param {Object} [object] Object to add the setter function to.
+ * @param {String} [name] Name of setter function to add.
+ * @param {Function} [setterFunction] Actual setter function. 
+ */
 module.exports.addSetter = function(object, name, setterFunction) {
   Object.defineProperty(object, name, {
     set: setterFunction,
@@ -45,6 +64,11 @@ module.exports.addSetter = function(object, name, setterFunction) {
   });
 };
 
+/**
+ * Checks whether dependencies required for bodyParser are installed.
+ *
+ * @return {Boolean}
+ */
 module.exports.bodyParserDependenciesInstalled = function() {
   try {
     require("copy-to");
@@ -56,13 +80,20 @@ module.exports.bodyParserDependenciesInstalled = function() {
   }
 };
 
-module.exports.setCharset = function setCharset(type, charset) {
-  if (!type || !charset) {
-    return type;
+/**
+ * Function to format Content-Type with different charset.
+ *
+ * @param {String} [header] Content-Type header to format.
+ * @param {String} [charset] Charset to format Content-Type header with.
+ * @return {String} Formatted content-type header with different charset.
+ */
+module.exports.setCharset = function setCharset(header, charset) {
+  if (!header || !charset) {
+    return header;
   }
 
   // parse type
-  let parsed = contentType.parse(type);
+  let parsed = contentType.parse(header);
 
   // set charset
   parsed.parameters.charset = charset;
