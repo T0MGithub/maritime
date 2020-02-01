83e4b33a4d5c03294fcc4dac724fa1202cc9f19a
Tom <emailfortomg@gmail.com>
2020-02-01 16:23:40 +0000

    Added data.cookies setter test

--- tests/data/data.cookie.js
+++ tests/data/data.cookie.js
@@ -4,6 +4,28 @@ const request = require("supertest");
 
 describe("data", function() {
   describe("cookies", function() {
+    describe("data.cookies = newObj", function() {
+      it("should set new cookies obj", function(done) {
+        const app = new Maritime();
+        const Cookies = require("cookies");
+
+        app.use(function(data) {
+          data.cookies.set("test", "test-data");
+          data.cookies = new Cookies(data.req, data.res);
+
+          // new cookies object shouldn't have test cookie because it has been replaced
+          assert(data.cookies.get("test") === undefined);
+
+          data.res.sendStatus(200);
+        });
+
+        request(app.listen())
+          .get("/")
+          .expect("Set-Cookie", "test=test-data; path=/; httponly")
+          .end(done);
+      });
+    });
+
     describe(".set(name, value)", function() {
       it("should set a cookie with specified name and value", function(done) {
         const app = new Maritime();
